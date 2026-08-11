1e372cbc455f4257a16ef1c7cdfd33ae6ab8615c
NSDeathman <nsdeathmanofficial@gmail.com>
2026-08-11 05:50:42 +0400

    Revert "Autotests for 3D vectors and fixes"
    This reverts commit 520963f9de7c4c9c00b6bfbbfae1255072249130.

--- autotests/main.cpp
+++ autotests/main.cpp
@@ -1,16 +1,12 @@
 #include "../include/xrMath.h"
-
 #include "xrTest.h"
 #include "test_vec2.h"
-#include "test_vec3.h"
 
 int main()
 {
     test_vector2_float();
+    test_vector2_double();
     test_vector2_int();
-
-    test_vector3_float();
-    test_vector3_int();
 
     return TestRunner::instance().printSummary();
 }
